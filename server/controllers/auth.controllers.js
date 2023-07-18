65842289ded66d1b543988a4852e45d48d6ab76a
Alejandroepilan <alejandroepilan@gmail.com>
2023-07-18 14:56:44 +0200

    token

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -60,3 +60,8 @@ export const logout = (req, res) => {
     })
     return res.sendStatus(200);
 };
+
+export const profile = (req, res) => {
+    console.log(req.user);
+    res.send('profile');
+}
