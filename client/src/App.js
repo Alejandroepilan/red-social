71781ee2d4464868a3b8f8797ff56cc8dd637b45
Alejandroepilan <alejandroepilan@gmail.com>
2023-07-18 11:55:46 +0200

    react + tailwind y express

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center">Hola</h1>
+    </div>
+  );
+}
+
+export default App;
